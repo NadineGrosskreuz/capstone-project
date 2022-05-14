9ef646766d365f058d0e8e67b0a0bc5a119bcf6f
Nadine Großkreuz <nadine.grosskreuz@web.de>
2022-05-14 16:00:55 +0200

    Add Typography component

---
+++ src/components/UI/Typography.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+export default function Typography({ children, variant, component, ...rest }) {
+	switch (variant) {
+		case 'h1':
+			return (
+				<StyledH1 {...rest} as={component}>
+					{children}
+				</StyledH1>
+			);
+		case 'h2':
+			return (
+				<StyledH2 {...rest} as={component}>
+					{children}
+				</StyledH2>
+			);
+		default:
+			return (
+				<StyledP {...rest} as={component}>
+					{children}
+				</StyledP>
+			);
+	}
+}
+
+const StyledH1 = styled.h1``;
+const StyledH2 = styled.h2`
+	font-size: 2.1rem;
+	text-transform: uppercase;
+	padding-bottom: 7px;
+`;
+
+const StyledP = styled.p`
+	font-size: 1.6rem;
+	padding-bottom: 12px;
+`;
