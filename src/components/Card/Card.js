9bf8bf114748893d2fe4e55c85f715a57f30add7
Nadine Großkreuz <nadine.grosskreuz@web.de>
2022-05-16 11:54:18 +0200

    Add function and styled-component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,16 +1,23 @@
+import { useState } from 'react';
+import { Accordion } from '../UI/Accordion.styled';
 import { CardContainer } from '../UI/CardContainer.styled';
 import Typography from '../UI/Typography';
 
 export default function Card({ category, name, address, products, information, visited, rating }) {
+	const [show, setShow] = useState(false);
+	const handleOpen = () => {
+		setShow(!show);
+	};
 	return (
-		<CardContainer>
+		<CardContainer onClick={handleOpen}>
 			<Typography variant="p">{category}</Typography>
 			<Typography variant="h2">{name}</Typography>
 			<Typography variant="p">{address}</Typography>
-			<Typography variant="p">{products}</Typography>
-			<Typography variant="p">{information}</Typography>
-			<Typography variant="p">{visited}</Typography>
-			<Typography variant="p">{rating}</Typography>
+			<Accordion>{show ? '-' : '+'}</Accordion>
+			{show && <Typography variant="p">{products}</Typography>}
+			{show && <Typography variant="p">{information}</Typography>}
+			{show && <Typography variant="p">{visited}</Typography>}
+			{show && <Typography variant="p">{rating}</Typography>}
 		</CardContainer>
 	);
 }
---
+++ src/components/UI/Accordion.styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Accordion = styled.button`
+	background-color: #573be9;
+	font-size: 1.6rem;
+	padding: 5px;
+	margin-bottom: 12px;
+	border-radius: 20px;
+	border: none;
+	width: 20%;
+`;
+
+export { Accordion };
