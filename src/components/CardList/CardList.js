2d9b243bace8b4bd0172956a80ba75955ea362c3
Nadine Großkreuz <nadine.grosskreuz@web.de>
2022-06-02 23:44:06 +0200

    Update CardList

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -5,12 +5,34 @@ import { CardGrid } from '../UI/CardGrid.styled';
 
 export default function CardList({ bookmarkMode }) {
 	const entries = useStore(state => state.entries);
+	const foundEntries = useStore(state => state.foundEntries);
 
 	if (bookmarkMode) {
 		const bookmarks = entries.filter(entry => entry.bookmark == true);
 		return (
 			<CardGrid>
 				{bookmarks.map(entry => (
+					<Card
+						key={entry.id}
+						id={entry.id}
+						category={entry.category}
+						name={entry.name}
+						address={entry.address}
+						products={entry.products}
+						information={entry.information}
+						visited={entry.visited}
+						rating={entry.rating}
+						edit={entry.edit}
+						bookmark={entry.bookmark}
+					/>
+				))}
+			</CardGrid>
+		);
+	} else if (foundEntries !== []) {
+		const search = entries.filter(entry => foundEntries.includes(entry) && entry);
+		return (
+			<CardGrid>
+				{search.map(entry => (
 					<Card
 						key={entry.id}
 						id={entry.id}
--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,52 +1,31 @@
 import { useState } from 'react';
 import useStore from '../useStore';
-import Card from '../Card/Card';
 
 export default function Searchbar() {
 	const [searchTerm, setSearchTerm] = useState('');
 	const findEntry = useStore(state => state.findEntry);
-	const foundEntries = useStore(state => state.foundEntries);
 
 	const handleChange = event => {
 		setSearchTerm(event.target.value);
 	};
 
 	return (
-		<>
-			<form
-				onSubmit={event => {
-					event.preventDefault();
-					findEntry(searchTerm);
-				}}
-			>
-				<label>
-					Search for flea markets/shops{' '}
-					<input
-						type="search"
-						placeholder="search for flea markets/shops"
-						value={searchTerm}
-						onChange={handleChange}
-					/>
-				</label>
-				<button type="submit">Suchen</button>
-			</form>
-			<ul>
-				{foundEntries.map(entry => (
-					<Card
-						key={entry.id}
-						id={entry.id}
-						category={entry.category}
-						name={entry.name}
-						address={entry.address}
-						products={entry.products}
-						information={entry.information}
-						visited={entry.visited}
-						rating={entry.rating}
-						edit={entry.edit}
-						bookmark={entry.bookmark}
-					/>
-				))}
-			</ul>
-		</>
+		<form
+			onSubmit={event => {
+				event.preventDefault();
+				findEntry(searchTerm);
+			}}
+		>
+			<label>
+				Search for flea markets/shops{' '}
+				<input
+					type="search"
+					placeholder="search for flea markets/shops"
+					value={searchTerm}
+					onChange={handleChange}
+				/>
+			</label>
+			<button type="submit">Suchen</button>
+		</form>
 	);
 }
