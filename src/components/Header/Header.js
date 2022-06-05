d0905c2ba10c70910325000a7724c17347476209
Nadine Großkreuz <nadine.grosskreuz@web.de>
2022-06-05 21:17:01 +0200

    Create header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,8 @@
+import Image from 'next/image';
+
+export default function Header() {
+	<header>
+		<Image src="logo.png" alt="Hand, die ein Peacezeichen macht, mit einem Herz dadrüber" />
+		<h1 />
+	</header>;
+}
