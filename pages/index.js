37fe9e50ee246a0fc8ea4fdfd06a4768741d6f6d
Nadine Großkreuz <nadine.grosskreuz@web.de>
2022-06-07 15:18:18 +0200

    Add more static data

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,8 @@
 import React from 'react';
 import { useState } from 'react';
 import dynamic from 'next/dynamic';
-import { getEntries } from '../src/services/get-entry';
 import { PageContainer } from '../src/components/UI/PageContainer.styled';
 import Searchbar from '../src/components/Searchbar/Searchbar';
-
-export function getStaticProps() {
-	const initialEntries = getEntries();
-	return {
-		props: {
-			initialEntries,
-		},
-	};
-}
 
 export default function Home() {
 	const CardList = dynamic(() => import('../src/components/CardList/CardList'), {
