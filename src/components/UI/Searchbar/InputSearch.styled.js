5ae0f8f0f74ef8fae5f6023023cee32486d617ee
Nadine Großkreuz <nadine.grosskreuz@web.de>
2022-06-08 23:10:48 +0200

    Adjust font input

--- src/components/UI/Searchbar/InputSearch.styled.js
+++ src/components/UI/Searchbar/InputSearch.styled.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
 const InputSearch = styled.input`
+	font-family: Arimo, sans-serif;
+	font-size: 1.6rem;
 	width: 95vw;
 	padding: 10px 35px 10px 15px;
 	border: none;
